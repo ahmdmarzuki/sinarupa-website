329773a4afd21130b2c4f7625c8c19d8f4159df3
ahmdmarzuki <ahmadmarzuki303@gmail.com>
2025-04-26 18:54:50 +0700

    setup page pre-event

---
+++ src/pages/PreEventHomepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PreEventHomepage = () => {
+  return (
+    <div>
+      <section>Navbar</section>
+      <section>Home</section>
+      <section>Latar belakan</section>
+    </div>
+  );
+};
+
+export default PreEventHomepage;

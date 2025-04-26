48594ae53a3d7d6e90f181fb736a1f7578b428e2
ahmdmarzuki <ahmadmarzuki303@gmail.com>
2025-04-26 22:12:15 +0700

    -

--- src/firebase/fingerPrint.js
+++
@@ -1,8 +0,0 @@
-// src/utils/fingerprint.js
-
-import FingerprintJS from "@fingerprintjs/fingerprintjs";
-
-// Promise yang akan meng-inisialisasi FingerprintJS sekali
-const fpPromise = FingerprintJS.load();
-
-export default fpPromise;

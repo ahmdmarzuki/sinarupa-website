7221632b2d02d97532f64f5fecddb6e9a4b304ad
ahmdmarzuki <ahmadmarzuki303@gmail.com>
2025-05-05 23:20:58 +0700

    -

--- src/sections/preEvent/LatarBelakang1.jsx
+++ src/sections/preEvent/LatarBelakang1.jsx
@@ -9,20 +9,19 @@ const LatarBelakang1 = () => {
   const isMobile = useMediaQuery("(max-width: 768px)");
 
   return (
-
     <div className="sm:mt-25 lg:mt-10 w-screen flex flex-col items-center py-17">
       <div className="relative w-full flex justify-center mb-17">
-              <BannerOrange className="w-[40vw] flex justify-center items-center">
-                <h1 className="font-oddval text-[#4A3D8F] lg:text-5xl sm:text-4xl">Latar Belakang</h1>
-              </BannerOrange>
-        </div>
-
+        <BannerOrange className="w-[40vw] flex justify-center items-center">
+          <h1 className="font-oddval text-[#4A3D8F] lg:text-5xl sm:text-4xl">
+            Latar Belakang
+          </h1>
+        </BannerOrange>
+      </div>
 
       <div className="mb-4 items-center">
-       {isMobile ? <LatarBelakangMobile /> : <LatarBelakang />}
-
+        {isMobile ? <LatarBelakangMobile /> : <LatarBelakang />}
       </div>
-    </AnimatedDiv>
+    </div>
   );
 };
 

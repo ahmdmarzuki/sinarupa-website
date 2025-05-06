c478dc009ba3de2a9d7c85ebbbe2b2c02ae82d4d
ahmdmarzuki <ahmadmarzuki303@gmail.com>
2025-05-06 08:21:49 +0700

    fixxx

--- src/sections/preEvent/LatarBelakang1.jsx
+++ src/sections/preEvent/LatarBelakang1.jsx
@@ -4,27 +4,30 @@ import LatarBelakangMobile from "../../components/LatarBelakangMobile";
 import BannerOrange from "../../components/BannerOrange";
 
 import { useMediaQuery } from "../../useMediaQuery";
+import AnimatedDiv from "../../components/AnimatedDiv";
 
 const LatarBelakang1 = () => {
   const isMobile = useMediaQuery("(max-width: 768px)");
 
   return (
-    <div
-      id="latar-belakang"
-      className="sm:mt-25 lg:mt-10 w-screen flex flex-col items-center py-17"
-    >
-      <div className="relative w-full flex justify-center mb-17">
-        <BannerOrange className="w-[40vw] flex justify-center items-center">
-          <h1 className="font-oddval text-[#4A3D8F] lg:text-5xl sm:text-4xl">
-            Latar Belakang
-          </h1>
-        </BannerOrange>
-      </div>
+    <AnimatedDiv>
+      <div
+        id="latar-belakang"
+        className="sm:mt-25 lg:mt-10 w-screen flex flex-col items-center py-17"
+      >
+        <div className="relative w-full flex justify-center mb-17">
+          <BannerOrange className="w-[40vw] flex justify-center items-center">
+            <h1 className="font-oddval text-[#4A3D8F] lg:text-5xl sm:text-4xl">
+              Latar Belakang
+            </h1>
+          </BannerOrange>
+        </div>
 
-      <div className="mb-4 items-center">
-        {isMobile ? <LatarBelakangMobile /> : <LatarBelakang />}
+        <div className="mb-4 items-center">
+          {isMobile ? <LatarBelakangMobile /> : <LatarBelakang />}
+        </div>
       </div>
-    </div>
+    </AnimatedDiv>
   );
 };
 
